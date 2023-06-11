dcd3e81bda18f2adf15ebff6d201b2c2fbf45632
Mathias1805 <mathias.principe@unmsm.edu.pe>
2023-06-11 11:22:46 -0500

    Actualizando Programa
    Agregando Proyecto hecho en Netbeans

---
+++ Proyecto_Base_De_Datos/src/proyecto/main.java
@@ -0,0 +1,23 @@
+
+package proyecto;
+
+/**
+ *
+ * @author MATHIAS
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login login = new Login();
+        Homepage home = new Homepage();
+        home.setVisible(true);
+        login.setVisible(false);
+        
+        
+    }
+    
+}
